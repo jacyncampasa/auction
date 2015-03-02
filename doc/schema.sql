94f7383b7b35a2d95e0752032ba92909747e18c6
Dexter Tad-y <dtady@cpan.org>
2015-03-02 10:37:12 +0800

    initial commit

---
+++ doc/schema.sql
@@ -0,0 +1,64 @@
+--
+
+create table if not exists auction_campaign (
+  id integer unsigned not null auto_increment,
+  code varchar(64) not null,
+  name varchar(255) not null,
+  date_begin date,
+  date_end date,
+  remarks text,
+  created datetime,
+  last_modified timestamp,
+  primary key (id),
+  unique index (code),
+  index (code)
+) engine=InnoDB default charset=utf8;
+
+
+create table if not exists auction_item (
+  id integer unsigned not null auto_increment,
+  campaign_code varchar(64) not null,
+  auction_date date not null,
+  auction_time_hhmm_begin varchar(4) not null,
+  auction_time_hhmm_end varchar(4) not null,
+  name varchar(255) not null,
+  remarks text,
+  created datetime,
+  last_modified timestamp,
+  primary key (id),
+  constraint foreign key (campaign_code) references auction_campaign (code) on update cascade,
+  index (campaign_code, auction_date)
+) engine=InnoDB default charset=utf8;
+
+
+
+-- automatic tables
+
+create table if not exists auction_offer (
+  id integer unsigned not null auto_increment,
+  item_id integer unsigned not null,
+  offer_amount decimal(9,2) not null,
+  created datetime,
+  last_modified timestamp,
+  primary key (id),
+  constraint foreign key (item_id) references auction_item (id),
+  unique index (item_id, offer_amount)
+) engine=InnoDB default charset=utf8;
+
+
+
+create table if not exists auction_bid (
+  id integer unsigned not null auto_increment,
+  offer_id integer unsigned not null,
+  msisdn varchar(32) not null,
+  cstamp varchar(32) not null,
+  is_first tinyint not null default 0,
+  is_outbid_sent tinyint null default null,
+  created datetime,
+  last_modified timestamp,
+  primary key (id),
+  constraint foreign key (offer_id) references auction_offer (id)
+) engine=InnoDB default charset=utf8;
+
+
+
---
+++ doc/use-cases.sql
@@ -0,0 +1,48 @@
+
+-- auction_am_alert_bcast.pl 
+-- params:
+--    campaign_code
+--    today_yyyymmdd
+
+--
+select * from auction_campaign where code="$campaign_code" and (date_begin <= "$today_yyyy-mm-dd") and (date_end >= "$today_yyyy-mm-dd");
+
+-- targets
+select * from autosubs where cautomediaid="$freebidalert-d" and lon=1
+  intersect VIP
+  sort uniq cstation
+
+
+-- A.M. bcast message is not unique per MIN, so it's okay to have this via usermsg interpolation
+
+select ai.id, ai.name
+  from 
+    auction_campaign as ac INNER JOIN auction_item as ai on (ac.code=ai.campaign_code) 
+    where 
+        ac.campaign_code="$campaign_code"
+    and ai.auction_date="$yest_yyyy-mm-dd"
+  ; -- get the item.id 
+
+select
+    ao.offer_amount as bid_amount, count(ab.id) as bid_count
+  from
+        auction_offer as ao INNER JOIN auction_bid as ab on (ao.id=ab.offer_id)
+  where
+        ao.item_id="$item_id"
+  group by
+        ab.id
+  having 
+        bid_count = 1
+  order by 
+        bid_amount ASC
+  ;
+
+-- targets file format: tab-delimited
+-- targets file fields:
+    -- msisdn 
+    -- 1dayago-item-name {YEST_ITEM_NAME}
+    -- 1dayago-item-awarded-offer-amount-x.xx {YEST_ITEM_AMT}
+    -- 0dayago-item-name {TODAY_ITEM_NAME}
+
+
+-- 
